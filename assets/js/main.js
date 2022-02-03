a87ae86e04d79bd3acc41ccc9f216550f43eab84
John Doe <islamsyahadad@gmail.com>
2022-02-03 17:13:27 +0700

    maintenance

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,7 +78,7 @@ function scrollActive()
             sectionTop = current.offsetTop - 58,
             sectionId = current.getAttribute('id')
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight ){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
         }else{
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
